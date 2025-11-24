593697231d5ddbea31b879972d12d982d238ab9e
Peter Sasse <psasse@gmx.de>
2025-11-24 14:17:10 +0100

    psasse initial

---
+++ src/main/java/org/psasse/Bid.java
@@ -0,0 +1,8 @@
+package org.psasse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bid {
+    public void updateLegalOrNaturalPerson() {}
+}
---
+++ src/main/java/org/psasse/Oms.java
@@ -0,0 +1,8 @@
+package org.psasse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Oms {
+    public void post() {}
+}
